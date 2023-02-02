b7b048301dae84a5a29b6ae4b854ed1d60a1b6ff
Nicholas Page <npage@southhills.edu>
2023-02-01 21:09:26 -0500

    Clarifications

--- IT100_Project2/IT100_Project2/Pages/Index.razor.cs
+++ IT100_Project2/IT100_Project2/Pages/Index.razor.cs
@@ -6,9 +6,14 @@ namespace IT100_Project2.Pages
     {
         private String helloWorld = String.Empty;
 
+        //Consider this method to be your Main() method.
         protected override void OnInitialized()
         {
             helloWorld = "Hello, world!";
+            //You will not be able to see Console.WriteLine()s in the terminal anymore.
+            //  To view Console.WriteLine()s, when the application is running in a browser, hit F12 (or FN + F12),
+            //  then go to the Console tab in the browser dev console.
+            Console.WriteLine("Testing, testing, 1 2 3.");
         }
     }
 }
