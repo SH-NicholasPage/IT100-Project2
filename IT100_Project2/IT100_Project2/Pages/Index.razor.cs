9ca2b8d09245dce0f671019cb79dee926deca25d
Nicholas Page <npage@southhills.edu>
2023-02-01 20:44:19 -0500

    Initial commit

---
+++ IT100_Project2/IT100_Project2/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IT100_Project2.Pages
+{
+    public partial class Index
+    {
+        private String helloWorld = String.Empty;
+
+        protected override void OnInitialized()
+        {
+            helloWorld = "Hello, world!";
+        }
+    }
+}
